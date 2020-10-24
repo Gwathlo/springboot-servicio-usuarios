c3db5080a0088d238894e53257ad9c890620a623
Gwathlo <Gwath@yahoo.com>
2020-10-24 22:17:09 +0200

    Unnecessary Repository

--- src/main/java/com/formacionbdi/springboot/app/usuarios/models/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.formacionbdi.springboot.app.usuarios.models.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.formacionbdi.springboot.app.usuarios.commons.models.entity.Role;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-
-}
