8b96bdd83cff96a76b841d3f7ab37fd85c0c9e91
Gwathlo <Gwath@yahoo.com>
2020-10-24 22:18:00 +0200

    Add encrypted passwords

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('user', '12345', 1, 'User', 'user', 'emailuser@email.com');
-INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '12345', 1, 'Admin', 'admin', 'emailAdmin@email.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('user', '$2a$10$GOk2QTNdsP9JBb6KPnI96eXl7r0oBujQ2d0909Ozp.YzjimUvuHR2', 1, 'User', 'user', 'emailuser@email.com');
+INSERT INTO usuarios (username, password, enabled, nombre, apellido, email) VALUES ('admin', '$2a$10$p89fJBZHTFuKYFut83xDK.Cua9wc60etsEXOLjuTwbcrOEGn9gzSu', 1, 'Admin', 'admin', 'emailAdmin@email.com');
 
 INSERT INTO roles (nombre) VALUES ('ROLE_USER');
 INSERT INTO roles (nombre) VALUES ('ROLE_ADMIN');
